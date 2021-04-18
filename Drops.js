97e7ccae2e15039c70898f9c46996d6d737fdea9
Vihaan-codes12 <77532722+Vihaan-codes12@users.noreply.github.com>
2021-04-18 11:27:06 +0100

    Update Drops.js

--- Drops.js
+++ Drops.js
@@ -1,9 +1,8 @@
 class createDrop {
     constructor(x,y){
         var options ={
-            restitution: 0.1,
             friction: 0.001,
-            isStatic: true
+            restitution: 0.1,
         }
         this.rain = Bodies.circle(x,y,5,options);
         this.radius = 5;
@@ -11,7 +10,7 @@ class createDrop {
     }
     updateY(){
         if(this.rain.position.y > height){
-            Matter.body.setPosition(this.rain, {x:random(0,400), y: random(0,400)})
+            Matter.Body.setPosition(this.rain, {x:random(0,400), y: random(0,400)})
         }
     }
     showDrop(){
